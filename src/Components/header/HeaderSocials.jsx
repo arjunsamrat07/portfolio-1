0c0c3422c8712a84263b4a9c6cad08d138b5f70b
arjunsamrat07 <arjun.samrat@u-smart.ai>
2023-03-01 17:42:47 +0530

    fifth commit

--- src/Components/header/HeaderSocials.jsx
+++ src/Components/header/HeaderSocials.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import{ImLinkedin} from "react-icons/im"
-import {FaGithub} from "react-icons/fa"
-import {AiFillInstagram} from "react-icons/ai"
-import {SiMedium} from "react-icons/si"
+import { ImLinkedin } from "react-icons/im"
+import { FaGithub } from "react-icons/fa"
+import { AiFillInstagram } from "react-icons/ai"
+import { SiMedium } from "react-icons/si"
 
 function HeaderSocials() {
   return (
     <div className='header__socials'>
-      <a href="https://www.linkedin.com/in/arjun-samrat-23b7b11aa/" target="blank"><ImLinkedin style={{fontSize:"20px"}} /></a>
-      <a href="https://github.com/arjun0705" target="blank"><FaGithub style={{fontSize:"20px"}}/></a>
-      <a href="https://medium.com/@arjunsamrat0705" target="blank"><SiMedium style={{fontSize:"20px"}}/></a>
-      <a href="https://www.instagram.com/_arjun0705_/" target="blank"><AiFillInstagram style={{fontSize:"20px"}}/></a>
+      <a href="https://www.linkedin.com/in/arjun-samrat-23b7b11aa/" target="blank" rel="noreferrer"><ImLinkedin style={{ fontSize: "20px" }} /></a>
+      <a href="https://github.com/arjun0705" target="blank" rel="noreferrer"><FaGithub style={{ fontSize: "20px" }} /></a>
+      <a href="https://medium.com/@arjunsamrat0705" target="blank" rel="noreferrer"><SiMedium style={{ fontSize: "20px" }} /></a>
+      <a href="https://www.instagram.com/_arjun0705_/" target="blank" rel="noreferrer"><AiFillInstagram style={{ fontSize: "20px" }} /></a>
     </div>
   )
 }
